1fef7a36b853a1270c24e2b96a750789402d4ff4
Antonino <antonino.praxedes@gmail.com>
2025-05-14 01:08:46 -0300

    feat: first commit H2 CRUD study fast

---
+++ src/main/java/com/github/antonino3g/produtosapi/ProdutosApiApplication.java
@@ -0,0 +1,14 @@
+package com.github.antonino3g.produtosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ProdutosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosApiApplication.class, args);
+	}
+
+}
